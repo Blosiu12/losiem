9e62d24acb01d1c4c0771aaf3faf80e455cf737c
Blosiu12 <97614945+Blosiu12@users.noreply.github.com>
2024-05-14 09:26:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("yeah!");
+
+
+const request = await fetch("https://cors.szurek.local/10/");
+let json = await response.json();
+
+if("error" in json){
+  alert(`Błąd${json["error"]}`)
+} else {
+  alert(`086a6338f9c2d3ac5c7ce22735b2922b`);
+}
